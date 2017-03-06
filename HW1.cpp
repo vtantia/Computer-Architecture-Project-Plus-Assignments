ca3abec808b230fb7a03c21a800a19898bf2e530
Vinayak Tantia <tantia.vinayak1@gmail.com>
2017-03-07 01:28:53 +0530

    Complete indirect call prediction

--- HW1.cpp
+++ HW1.cpp
@@ -5,6 +5,12 @@
 
 void Exit();
 
+struct BtbEntry {
+  BOOL valid;
+  ADDRINT tag, target;
+  UINT64 timestamp;
+};
+
 /* ================================================================== */
 // Global variables
 /* ================================================================== */
@@ -17,6 +23,12 @@ UINT64 run_inst_count = 1000000000;
 UINT32 mispredFNBT = 0;
 UINT32 mispredBimod = 0;
 UINT32 bimod[512] = {0};
+
+#define NUM_BTB 128
+UINT32 mispredBTB[2] = {0};
+UINT32 lastTimeBTB[2] = {0};
+UINT32 btbHist = 0;
+BtbEntry btb[2][NUM_BTB][4];
 
 std::ostream *out = &cerr;
 
@@ -62,6 +74,55 @@ VOID BranchPredFNBT(ADDRINT insAddr, ADDRINT bTargetAddr, BOOL isTaken) {
   updateCount(&bimod[index], isTaken, 3);
 }
 
+VOID UpdateCondBrHist(BOOL isTaken) {
+  btbHist = (btbHist << 1) % NUM_BTB;
+  btbHist += isTaken;
+}
+
+VOID IndirectPred(ADDRINT insAddr, ADDRINT nextInsAddr, ADDRINT targetAddr,
+                  UINT32 cacheIndex, UINT32 btbIndex) {
+  UINT32 invalidIndex = 4, lruIndex = 4;
+  BOOL found = false;
+
+  for (UINT32 i = 0; i < 4; i++) {
+    BtbEntry &b = btb[btbIndex][cacheIndex][i];
+
+    if (!b.valid) {
+      invalidIndex = i;
+    } else if (b.tag == insAddr) {
+      mispredBTB[btbIndex] += !(targetAddr == b.target);
+      b.target = targetAddr;
+      b.timestamp = lastTimeBTB[btbIndex]++;
+      found = true;
+      break;
+    } else if (lruIndex == 4 ||
+               b.timestamp <
+                   btb[btbIndex][cacheIndex][lruIndex].timestamp) {
+      lruIndex = i;
+    }
+  }
+
+  UINT32 toUpdate = (invalidIndex == 4) ? lruIndex : invalidIndex;
+  if (!found && targetAddr != nextInsAddr) {
+    mispredBTB[btbIndex] += 1;
+
+    btb[btbIndex][cacheIndex][toUpdate] = {true, insAddr, targetAddr,
+                                           lastTimeBTB[btbIndex]++};
+    /*BtbEntry &b =*/ // b.valid = true;
+    // b.tag = insAddr;
+    // b.target = targetAddr;
+  }
+}
+
+VOID IndirectPredBTB(ADDRINT insAddr, ADDRINT nextInsAddr,
+                     ADDRINT targetAddr) {
+  UINT32 cacheIndex = (insAddr % NUM_BTB);
+  IndirectPred(insAddr, nextInsAddr, targetAddr, cacheIndex, 0);
+
+  cacheIndex = cacheIndex ^ (btbHist);
+  IndirectPred(insAddr, nextInsAddr, targetAddr, cacheIndex, 1);
+}
+
 ADDRINT Terminate(void) {
   return (insCount >= fast_forward_count + run_inst_count);
 }
@@ -69,13 +130,17 @@ ADDRINT Terminate(void) {
 // Analysis routine to check fast-forward condition
 ADDRINT FastForward(void) { return (insCount >= fast_forward_count); }
 
-/* ===================================================================== */
+/* =====================================================================
+ */
 // Analysis routines
-/* ===================================================================== */
+/* =====================================================================
+ */
 
-/* ===================================================================== */
+/* =====================================================================
+ */
 // Instrumentation callbacks
-/* ===================================================================== */
+/* =====================================================================
+ */
 
 VOID Instruction(INS ins, VOID *v) {
   if (INS_IsDirectBranch(ins)) {
@@ -85,6 +150,24 @@ VOID Instruction(INS ins, VOID *v) {
         INS_Address(ins),        // instruction address
         IARG_BRANCH_TARGET_ADDR, // branch target address
         IARG_BRANCH_TAKEN,       // if branch is taken
+        IARG_END);
+  }
+
+  if (INS_Category(ins) == XED_CATEGORY_COND_BR) {
+    INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)FastForward, IARG_END);
+    INS_InsertThenPredicatedCall(
+        ins, IPOINT_BEFORE, (AFUNPTR)UpdateCondBrHist,
+        IARG_BRANCH_TAKEN, // branch target address
+        IARG_END);
+  }
+
+  if (INS_IsCall(ins) && INS_IsIndirectBranchOrCall(ins)) {
+    INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)FastForward, IARG_END);
+    INS_InsertThenPredicatedCall(
+        ins, IPOINT_BEFORE, (AFUNPTR)IndirectPredBTB, IARG_ADDRINT,
+        INS_Address(ins),                   // instruction address
+        IARG_ADDRINT, INS_NextAddress(ins), // next ins addr
+        IARG_BRANCH_TARGET_ADDR,            // branch target address
         IARG_END);
   }
 
@@ -110,6 +193,9 @@ void Exit() {
   *out << "===============================================" << endl;
   *out << "Mispredictions in FNBT : " << mispredFNBT << endl;
   *out << "Mispredictions in bimodal : " << mispredBimod << endl;
+  *out << "Mispredictions in BTB PC : " << mispredBTB[0] << endl;
+  *out << "Mispredictions in BTB PC and global history : " << mispredBTB[1]
+       << endl;
   *out << "===============================================" << endl;
   exit(0);
 }
