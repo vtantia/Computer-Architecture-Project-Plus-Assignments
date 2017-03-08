2ce69b92661249244855c66e0503bc32f2632772
Pallav Agarwal <pallavag@iitk.ac.in>
2017-03-09 01:00:37 +0530

    Separate count for forward and backward branches

--- HW2.cpp
+++ HW2.cpp
@@ -16,36 +16,38 @@ struct BtbEntry {
 /* ================================================================== */
 
 UINT64 insCount = 0; // number of dynamically executed instructions
+UINT64 totPreds = 0;
+UINT64 directionCount[2] = {0};
 
 UINT64 fast_forward_count = 30;
 UINT64 run_inst_count = 1000000000;
 
-UINT32 mispredFNBT = 0;
+UINT32 mispredFNBT[3] = {0};
 //
-UINT32 mispredBimod = 0;
+UINT32 mispredBimod[3] = {0};
 UINT32 bimod[512] = {0};
 //
 UINT32 sag_bht[1024] = {0};
 UINT32 sag_pht[512] = {0};
-UINT32 mispredSag = 0;
+UINT32 mispredSag[3] = {0};
 //
 UINT32 g_bht = 0;
 UINT32 gag_pht[512] = {0};
-UINT32 mispredGag = 0;
+UINT32 mispredGag[3] = {0};
 //
 UINT32 gshare_pht[512] = {0};
-UINT32 mispredGshare = 0;
+UINT32 mispredGshare[3] = {0};
 //
-UINT32 mispredHyb1 = 0;
+UINT32 mispredHyb1[3] = {0};
 UINT32 hyb1_sag_bht[1024] = {0};
 UINT32 hyb1_sag_pht[512] = {0};
 UINT32 hyb1_gag_bht = 0;
 UINT32 hyb1_gag_pht[512] = {0};
 UINT32 hyb1_meta[512] = {0};
 //
-UINT32 mispredHyb2maj = 0;
+UINT32 mispredHyb2maj[3] = {0};
 //
-UINT32 mispredHyb2meta = 0;
+UINT32 mispredHyb2meta[3] = {0};
 UINT32 hyb2_s_g[512] = {0};
 UINT32 hyb2_s_gsh[512] = {0};
 UINT32 hyb2_g_gsh[512] = {0};
@@ -94,30 +96,52 @@ inline VOID updateCount(UINT32 *toChange, BOOL isTaken, UINT32 limit) {
 }
 
 VOID BranchPredFNBT(ADDRINT insAddr, ADDRINT bTargetAddr, BOOL isTaken) {
-  mispredFNBT += (bTargetAddr > insAddr) ? isTaken : !isTaken;
+  totPreds++;
+  BOOL isForward = (bTargetAddr > insAddr);
+  directionCount[isForward]++;
 
-  mispredBimod += (bimod[(insAddr) % 512] < 2) ? isTaken : !isTaken;
+  mispredFNBT[0] += isForward * (isForward ? isTaken : !isTaken);
+  mispredFNBT[1] += (!isForward) * (isForward ? isTaken : !isTaken);
+  mispredFNBT[2] += isForward ? isTaken : !isTaken;
+
+  mispredBimod[0] +=
+      isForward * ((bimod[(insAddr) % 512] < 2) ? isTaken : !isTaken);
+  mispredBimod[1] +=
+      (!isForward) * ((bimod[(insAddr) % 512] < 2) ? isTaken : !isTaken);
+  mispredBimod[2] += (bimod[(insAddr) % 512] < 2) ? isTaken : !isTaken;
   updateCount(&bimod[(insAddr) % 512], isTaken, 3);
 
   UINT32 hist_sag = sag_bht[insAddr % 1024];
   UINT32 sag_pred = !(sag_pht[hist_sag] < 2);
-  mispredSag += (sag_pred) ? !isTaken : isTaken;
+  mispredSag[0] += isForward * ((sag_pred) ? !isTaken : isTaken);
+  mispredSag[1] += (!isForward) * ((sag_pred) ? !isTaken : isTaken);
+  mispredSag[2] += (sag_pred) ? !isTaken : isTaken;
   updateCount(&sag_pht[hist_sag], isTaken, 3);
   sag_bht[insAddr % 1024] =
       ((sag_bht[insAddr % 1024] << 1) + isTaken) & 0x1ff;
 
   UINT32 gag_pred = !(gag_pht[g_bht] < 4);
-  mispredGag += (gag_pred) ? !isTaken : isTaken;
+  mispredGag[0] += isForward * ((gag_pred) ? !isTaken : isTaken);
+  mispredGag[1] += (!isForward) * ((gag_pred) ? !isTaken : isTaken);
+  mispredGag[2] += (gag_pred) ? !isTaken : isTaken;
   updateCount(&gag_pht[g_bht], isTaken, 7);
 
   UINT32 index = g_bht ^ (insAddr & 0x1ff);
   UINT32 gshare_pred = !(gshare_pht[index] < 4);
-  mispredGshare += (gshare_pred) ? !isTaken : isTaken;
+  mispredGshare[0] += isForward * ((gshare_pred) ? !isTaken : isTaken);
+  mispredGshare[1] += (!isForward) * ((gshare_pred) ? !isTaken : isTaken);
+  mispredGshare[2] += (gshare_pred) ? !isTaken : isTaken;
   updateCount(&gshare_pht[index], isTaken, 7);
 
   UINT32 needUpdate = ((!gag_pred) * sag_pred) + (gag_pred * (!sag_pred));
-  mispredHyb1 += (hyb1_meta[g_bht] < 2) ? (isTaken != sag_pred)
-                                        : (isTaken != gag_pred);
+  mispredHyb1[0] +=
+      isForward * ((hyb1_meta[g_bht] < 2) ? (isTaken != sag_pred)
+                                          : (isTaken != gag_pred));
+  mispredHyb1[1] +=
+      (!isForward) * ((hyb1_meta[g_bht] < 2) ? (isTaken != sag_pred)
+                                             : (isTaken != gag_pred));
+  mispredHyb1[2] += (hyb1_meta[g_bht] < 2) ? (isTaken != sag_pred)
+                                           : (isTaken != gag_pred);
   if (needUpdate) {
     // GAg is assumed on high end of counter
     UINT32 gagCorrect = (gag_pred == isTaken);
@@ -125,7 +149,9 @@ VOID BranchPredFNBT(ADDRINT insAddr, ADDRINT bTargetAddr, BOOL isTaken) {
   }
 
   UINT32 majVote = (gshare_pred + gag_pred + sag_pred) > 1;
-  mispredHyb2maj += (majVote != isTaken);
+  mispredHyb2maj[0] += isForward * ((majVote != isTaken));
+  mispredHyb2maj[1] += (!isForward) * ((majVote != isTaken));
+  mispredHyb2maj[2] += (majVote != isTaken);
 
   UINT32 metaVote = 0;
   if (hyb2_s_g[g_bht] < 2 && hyb2_s_gsh[g_bht] < 2) {
@@ -137,7 +163,9 @@ VOID BranchPredFNBT(ADDRINT insAddr, ADDRINT bTargetAddr, BOOL isTaken) {
   } else {
     metaVote = (gshare_pred + gag_pred + sag_pred) > 1;
   }
-  mispredHyb2meta += (metaVote != isTaken);
+  mispredHyb2meta[0] += isForward * ((metaVote != isTaken));
+  mispredHyb2meta[1] += (!isForward) * ((metaVote != isTaken));
+  mispredHyb2meta[2] += (metaVote != isTaken);
   UINT32 update_s_g = ((!gag_pred) * sag_pred) + (gag_pred * (!sag_pred));
   UINT32 update_s_gsh =
       ((!gshare_pred) * sag_pred) + (gshare_pred * (!sag_pred));
@@ -278,6 +306,11 @@ void Exit() {
   *out << "MyPinTool analysis results: " << endl;
   *out << "Number of instructions: " << insCount << endl;
   *out << "===============================================" << endl;
+  *out << "Total number of predictions tried: " << totPreds << endl;
+  *out << "Total number of predictions tried for forward: "
+       << directionCount[1] << endl;
+  *out << "Total number of predictions tried for backward: "
+       << directionCount[0] << endl;
   *out << " & FNBT & Bimodal & SAg & GAg & gshare & Hybrid of SAg and GAg "
           "& Hybrid of All: Majority Voter & Hybrid of All: Three Tables"
        << endl;
@@ -286,14 +319,24 @@ void Exit() {
        << " & " << mispredGshare << " & " << mispredHyb1 << " & "
        << mispredHyb2maj << " & " << mispredHyb2meta << endl;
   *out << "===============================================" << endl;
-  *out << "Mispredictions in FNBT : " << mispredFNBT << endl;
-  *out << "Mispredictions in bimodal : " << mispredBimod << endl;
-  *out << "Mispredictions in SAg : " << mispredSag << endl;
-  *out << "Mispredictions in GAg : " << mispredGag << endl;
-  *out << "Mispredictions in Gshare : " << mispredGshare << endl;
-  *out << "Mispredictions in Hybrid1 : " << mispredHyb1 << endl;
-  *out << "Mispredictions in Hybrid2-1 : " << mispredHyb2maj << endl;
-  *out << "Mispredictions in Hybrid2-2 : " << mispredHyb2meta << endl;
+  *out << "Mispredictions in FNBT : " << mispredFNBT[0] << " "
+       << mispredFNBT[1] << " " << mispredFNBT[2] << endl;
+  *out << "Mispredictions in bimodal : " << mispredBimod[0] << " "
+       << mispredBimod[1] << " " << mispredBimod[2] << endl;
+  *out << "Mispredictions in SAg : " << mispredSag[0] << " "
+       << mispredSag[1] << " " << mispredSag[2] << endl;
+  *out << "Mispredictions in GAg : " << mispredGag[0] << " "
+       << mispredGag[1] << " " << mispredGag[2] << endl;
+  *out << "Mispredictions in Gshare : " << mispredGshare[0] << " "
+       << mispredGshare[1] << " " << mispredGshare[2] << endl;
+  *out << "Mispredictions in Hybrid1 (SAg+GAg): " << mispredHyb1[0] << " "
+       << mispredHyb1[1] << " " << mispredHyb1[2] << endl;
+  *out << "Mispredictions in Hybrid2-1 (SAg+GAg+gshare, majority): "
+       << mispredHyb2maj[0] << " " << mispredHyb2maj[1] << " "
+       << mispredHyb2maj[2] << endl;
+  *out << "Mispredictions in Hybrid2-2 (SAg+GAg+gshare, metameta): "
+       << mispredHyb2meta[0] << " " << mispredHyb2meta[1] << " "
+       << mispredHyb2meta[2] << endl;
   *out << "===============================================" << endl;
   *out << "BTB PC:" << endl;
   outputBTB(0);
